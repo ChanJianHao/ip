b0145ac0e75eeef1bad84c8c6305a473f1726696
ChanJianHao <49814823+ChanJianHao@users.noreply.github.com>
2020-08-16 14:17:36 +0800

    Revert "Add Level 0. Greet"
    This reverts commit 9d07e18b95a6c701b7605b9d457cf02237af0932.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,10 @@
 public class Duke {
     public static void main(String[] args) {
-
-        // Print logo
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(logo);
-
-        // DUKE Greet
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Duke");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
-
+        System.out.println("Hello from\n" + logo);
     }
 }
