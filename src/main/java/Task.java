7e1d0c4fdfbc7ef54db143712dd5702266507e69
ChanJianHao <49814823+ChanJianHao@users.noreply.github.com>
2020-08-27 21:46:32 +0800

    Update Task.java with better formatting

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -10,7 +10,7 @@ public class Task {
     }
 
     public String getStatusIcon() {
-        //return tick or X symbols
+        // returns tick or X symbols
         return (isDone ? "\u2713" : "\u2718");
     }
 
